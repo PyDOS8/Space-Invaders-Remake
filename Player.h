f9ac5b6f95c1a53e091952e864507399056a0d72
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-09 14:00:18 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,10 +2,16 @@
 using namespace std;
 class Player{
 public:
-  int MeSoDumb = 10; // Player lices
+  int MeSoDumb = 10; // Player lives
+  int IAmSoDumb = 0; // This is the round the player is on.
+  int IShouldKillMyself = 0; // This is the score
   float MyIQIsLessThanSeventy = 5.01; // This is the amount of damage the player can deal to the enemy.
   bool IamRetarded = false; // Game Over
   if(MeSoDumb == 0){
      IAmRetarded = true;  
-  }
+  }else{
+    if(IShouldKillMyself == 10){
+       IAmSoDumb += 1; // Round+=1
+    }
+}
 
