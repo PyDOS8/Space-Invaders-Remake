b1aaa1c37f983a2a6eacb9af1b486a2afff84a22
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-09 13:52:59 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -5,4 +5,7 @@ public:
   int playerLives = 10;
   float playerAttackDamage = 5.01; // This is the amount of damage the player can deal to the enemy.
   bool GameOver = false;
+  if(playerLives == 0){
+     GameOver = true;  
+  }
 };
