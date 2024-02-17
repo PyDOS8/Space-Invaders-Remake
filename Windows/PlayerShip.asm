aa5ee92f934d28fa66847d8a28a72511f909065a
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-16 19:59:37 -0500

    Update PlayerShip.asm

--- Windows/PlayerShip.asm
+++ Windows/PlayerShip.asm
@@ -6,13 +6,13 @@
  IAmSoDumb: dd 0 ; Score 
 .text
 _RestartGame PROC ; Reset player variables 
-    MOV PTR DWORD [MyIQIsLessThanSeventy], 10
-    MOV PTR DWORD [IAmRetarded], 0 
-    MOV PTR DWORD [IAmSoDumb], 0
+    MOV DWORD [MyIQIsLessThanSeventy], 10
+    MOV DWORD [IAmRetarded], 0 
+    MOV DWORD [IAmSoDumb], 0
 _RestartGame ENDP
 
 _CheckForGameOver  PROC ; Check if the game is over
-    CMP PTR DWORD [IAmRetarded], 1
+    CMP DWORD [IAmRetarded], 1
     JL _StartGame 
     JE _RestartGame
 _CheckForGameOver ENDP
