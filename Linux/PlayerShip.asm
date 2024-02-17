dc9e8df305a44e6c4187eb92c6717d87f4e3ce72
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-16 20:13:08 -0500

    Update PlayerShip.asm

--- Linux/PlayerShip.asm
+++ Linux/PlayerShip.asm
@@ -1,26 +1,22 @@
 section .text
- global _IAmSoStupid           ; Collision Detection for the Y axis
- global _IHaveAnIQBelowSeventy ; Collision Detection for the X axis
- global _WhyWontIDie           ; Check if the player has died
- global _IWillKillMyself       ; Check if we can reset the game
- global _IAmSoDumb             ; Reset the game
+ global _RestartGame
+ global _IncreaseScore 
+ global _IncludeMacros
 section .data
-  IAmDumb: dd 0 ; Player Y Axis
-  IHateMyself: dd 0 ; Player X Axis
-  IShouldKillMyself: dd 10  ; Player lives
-  IHaveAnIQBelowFifty: dd 0 ; Times Reseted
+  playerLives: dd 10
+  score: dd  0
+  GameOver: dd 0   
 
-_WhyWontIDie:
-  cmp [IShouldKillMyself], 0
-  je _IWillKillMyself  ; If the lives are equal to 0, goto IWillKillMyself
+_RestartGame:
+  mov dword [playerLives], 10 
+  mov dword [score], 0
+  mov dword [GameOver], 0 
 
-_IWillKillMyself:
-  cmp dword [IHaveAnIQBelowFifty], 10
-  jle _IAmSoDumb
+_IncludeMacros:
+ %macro CheckIfWeCanIncreaseTheScore 1
+   cmp dword [score], 10
+   jl _%1
+ endmacro%
 
-; Reset player variables
-_IAmSoDumb:
-  mov dword [IAmDumb], 0 ; Player Y Axis = 0
-  mov dword [IHateMyself], 0 ; Player X axis = 0
-  mov dword [IShouldKillMyself], 10 ; Player Lives = 10
-  mov dword [IHaveAnIQBelowFifty], 0 ; Times Reseted = 0
+_IncreaseScore:
+  add dword [score], 1
