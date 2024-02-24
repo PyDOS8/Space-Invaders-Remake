a89a111f0215d471fad5e281b7c5b84bc3ee35ef
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-23 23:09:09 -0500

    Update and rename Assembler.py to Assembler.cpp

---
+++ Assembler.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+using namespace std;
+int main(){   
+  // CPU machine code instructions
+  map<string, int>IAmSoDumb = {
+        {"IHateMyself": 114000},
+  };
+  return 0;
+}
