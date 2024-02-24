3903709deb85a86c0d01b1088c0ca0edafad30fe
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-24 12:30:46 -0500

    Update PlayerShip.asm

--- Windows/PlayerShip.asm
+++ Windows/PlayerShip.asm
@@ -1,20 +1,21 @@
-.386
-.model stack, flat
-.data
+section .text
+ global _RestartGame 
+section .data
  MyIQIsLessThanSeventy: dd 10 ; Player lives
  IAmRetarded: dd 0 ; Game Over
  IAmSoDumb: dd 0 ; Score 
-.text
-_RestartGame PROC ; Reset player variables 
-    MOV DWORD [MyIQIsLessThanSeventy], 10
-    MOV DWORD [IAmRetarded], 0 
-    MOV DWORD [IAmSoDumb], 0
-_RestartGame ENDP
-_StartGame PROC
-  
-_StartGame ENDP
-_CheckForGameOver  PROC ; Check if the game is over
-    CMP  [IAmRetarded], 1
-    JL _StartGame 
-    JE _RestartGame
-_CheckForGameOver ENDP
+section .text
+  global _RestartGame 
+  global _StartGame
+  global _CheckForGameOver
+  global _ExitGame
+
+_CheckForGameOver:
+  cmp dword [IAmRetarded], 1
+  je _RestartGame
+  jl _StartGame
+
+_ExitGame:
+  extern _ExitProcess@4
+  push 0
+  call _ExitProcess@4
