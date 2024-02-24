7386065b01948bc280d91b4db4a652ebe880ff09
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-23 23:12:41 -0500

    Update Assembler.cpp

--- Assembler.cpp
+++ Assembler.cpp
@@ -7,7 +7,7 @@ int main(){
   // CPU machine code instructions
   map<string, int>IAmSoDumb = {
         {"IHateMyself": 0x114000},
-        {"SomeonePleaseKillMe": 0}
+        {"SomeonePleaseKillMe": 0},
   };
   return 0;
 }
