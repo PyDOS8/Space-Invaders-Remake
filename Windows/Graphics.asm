33b607e00c4f7d1afe45b640e360ba7b1c4d0fac
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-25 14:25:40 -0500

    Update Graphics.asm

--- Windows/Graphics.asm
+++ Windows/Graphics.asm
@@ -1,25 +1,75 @@
-section .data
- mainMenuTop:   db "***************************************************"
- mainMenuTopLength: equ $- mainMenuTop 
- mainMenuEnd: db "***************************************************"
- mainMenuEndLength: equ $ - mainMenuEnd
 section .text
- global _DrawMainMenu
- global _exit
+ global _DrawLeftAndRightSide 
+ global _ColorLeftSide
+ global _ColorRightSide
+ global _DrawTopAndBottomSide
 
-_DrawMainMenu:
- ; Draw the top part of the main menu
- extern _printf
- push mainMenuTop
- push mainMenuTopLength
- call _printf
+section .data
+	LeftMainMenu: db "*"                        
+	LeftMainMenuLength: equ $ - LeftMainMenu
+	timesDrawn: dd 0
+	newLine: dd 10
+	RightMainMenu: db "*"
+	RightMenuLength: equ $ - RightMainMenu
+	colorCommand: db "color "
+	playerSpaceShip: db "/\'"
 
- ; Draw the bottom part of the main menu
- push mainMenuEnd
- push mainMenuEndLength
- call _printf
+_DrawTopAndBottomSide:
+    extern _WriteConsole
 
-_exit:
- extern _ExitProcess@4
- push 0 ; Exit code (0) Sucessful exit
- call _ExitProcess@4
+	; Draw the Top part of the main menu
+	push TopMainMenu
+	push TopMainMenuLength
+	call _WriteConsole
+
+	; Draw the bottom part of the main menu
+	push LeftMainMenu
+	push LeftMainMenuLength
+	call _WriteConsole
+
+_DrawLeftSide:
+
+	; Positon the left side of the menu
+	push 200
+	call _SetCursorPosition 
+
+	; Draw the blank line
+	push newLine 
+	push 2
+	call _WriteConsole
+
+	; Check if we need to draw again
+	add dword [timesDrawn], 1
+	cmp dword [timesDrawn], 50
+	jg _DrawRightSide 
+	jle _DrawLeftSide
+
+_DrawLeftAndRightSide:
+	
+	; Draw the left side of the top menu
+	push LeftMainMenu
+	push LeftMainMenuLength
+	call _WriteConsole 
+
+	; Position the rigt side of the main menu 
+	push newLine 
+	push -200
+	call _WriteConsole 
+
+	; Draw the blank line
+	push newLine
+	push 2
+	call _WriteConsole
+
+	; Check if we need to draw again
+	add dword [timesDrawn], 1
+	cmp dword [timesDrawn], 100
+	jle _DrawRightSide 
+	jg _DrawPlayer
+
+_DrawPlayer:
+
+	; Draw the player space ship
+	push playerSpaceShip
+	push 3
+	call _WriteConsole
