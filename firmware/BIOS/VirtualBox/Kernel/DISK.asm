3886d7c91d411eb8e9f87271d35121d32b73501e
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-26 11:37:53 -0500

    Update DISK.asm

--- firmware/BIOS/VirtualBox/Kernel/DISK.asm
+++ firmware/BIOS/VirtualBox/Kernel/DISK.asm
@@ -2,11 +2,19 @@ bits 16
 section .text
  global _ResetDisk
  global _CheckDriveStatus
+ global _WriteSectors
 
 _ResetDisk: ; This function will make the disk go to its original form
  MOV AH, 00h
  INT 13h
+ RET
 
-_CheckDriveStatus: ; This function will check the drivers status
+_CheckDriveStatus: ; This function will check the drivers status (eg. full or empty)
  MOV AH, 01h
  INT 13h
+ RET
+
+_WriteSector: ; Write to the sector
+ MOV AH, 01h
+ INT 13h
+ RET
