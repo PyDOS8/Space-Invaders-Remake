e21bbb79fd97934fe65ab1954805bf935f30392a
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-29 18:54:33 -0500

    Create bootloader.asm

---
+++ Kernel/bootloader.asm
@@ -0,0 +1,16 @@
+.CODE
+
+ORG 0x00000000
+.8086 ; Intel 8086 (X86 16 bit processor)
+.TEXT 
+    _START PROC 
+        MOV CX, [0x00000000] ; Hold the data in the RAM 
+        
+        ; Check if the RAM is empty
+        CMP CX, 0
+        JG _BootGame 
+    ENDP
+
+    _BootGame PROC 
+        JMP CX
+    _BootGame ENDP
