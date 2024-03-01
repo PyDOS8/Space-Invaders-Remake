9b013a905c90e2d018a61728bd49a001ed68db4a
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-02-29 19:08:54 -0500

    Update bootloader.asm

--- Kernel/bootloader.asm
+++ Kernel/bootloader.asm
@@ -5,7 +5,7 @@ ORG 0x00000000
 .8086 ; Intel 8086 (X86 16 bit processor)
 .TEXT 
     _START PROC 
-        MOV CX, [0x00000000] ; Hold the data in the RAM 
+        MOV CX, [0XFFFF] ; Hold the data in the RAM 
         
         ; Check if the RAM is empty
         CMP CX, 0
