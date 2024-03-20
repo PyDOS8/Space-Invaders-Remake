a573096f5dd0b6f244f89feb70c8c4c27a454a6f
AssemblyProgrammer101 <124309800+PyDOS8@users.noreply.github.com>
2024-03-20 06:20:49 -0400

    Update Game.java

--- OS/Game.java
+++ OS/Game.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.lang.Thread;
 
-public class Main
+public class Game
 {
     public static Scanner sc = new Scanner(System.in);
     
@@ -16,7 +16,7 @@ public class Main
                 System.out.print(line.substring(count,count+1));
                 Thread.sleep(50);
 	        }
-            }
+        }
         catch (Exception e) {
            
             System.out.println(e);
@@ -24,62 +24,70 @@ public class Main
     }
 
 	public static void clearScreen() {  
-	System.out.print("\033[H\033[2J");  
-    System.out.flush();  
+		System.out.print("\033[H\033[2J");  
+		System.out.flush();  
    }
    
 	public static void Home() {
-	        clearScreen();
-	System.out.println("Welcome to Space Invaders");
-	
-	System.out.println("to Start\t press 1");
-	System.out.println("For Settings\t press 2");
-	System.out.println("For Credits\t press 3");
-	System.out.println("For the Rules\t press 4");
-	int input = sc.nextInt();
-	clearScreen();
-	switch (input){
+	    clearScreen();
+		System.out.println("Welcome to Space Invaders");
+		
+		System.out.println("to Start\t press 1");
+		System.out.println("For Settings\t press 2");
+		System.out.println("For Credits\t press 3");
+		System.out.println("For the Rules\t press 4");
+		int input = sc.nextInt();
+		clearScreen();
+		switch (input){
 	   
-	    case 1: Game();
-	    break;
-	    case 2: Settings();
-	    break;
-	    case 3: Credits();
-	    break;
-	    case 4: Rules();
-	    break;
+			case 1: Game();
+				break;
+			case 2: 
+				Settings();
+				break;
+			case 3: 
+				Credits();
+				break;
+			case 4: 
+				Rules();
+				break;
+		}
 	}
-	}
+
 	public static void Rules() {
-	System.out.println("Don't die.");
-    clearScreen();
-	System.out.println("To Leave\t press 2");
+		System.out.println("Don't die.");
+		clearScreen();
+		System.out.println("To Leave\t press 2");
+		int input = sc.nextInt();
+		if(input == 2){ 
+			Home();
+		}
+}
+public static void Credits() {
+	String line = "";
+	line = "By Ali Al-Nasih, & Ethan Mullally";
+	SlowText(line);
+	System.out.print("\n");
+	System.out.println("Return Home (Any number except 0)");
 	int input = sc.nextInt();
-	if(input == 2){ 
-	    Home();
+	if (input < 0 || input > 0){
+		Home();
 	}
 }
-    public static void Credits() {
-        String line = "";
-        line = "By Ali Al-Nasih, & Ethan Mullally";
-        SlowText(line);
-        System.out.print("\n");
-        System.out.println("Return Home (Any number except 0)");
-        int input = sc.nextInt();
-        if (input < 0 || input > 0){Home();}
-    }
-    public static void Settings() {
-        String line = "Nothing To Change When its perfect.";
-        SlowText(line);
-        System.out.println("\nReturn Home (Any number except 0)");
-        int input = sc.nextInt();
-        if (input < 0 || input > 0){Home();}
-    }
-    
-    public static void Game() {
-        String line = "Nothing here yet.";
-        SlowText(line);
-        System.out.println("\nReturn Home (Any number except 0)");
-        int input = sc.nextInt();
-    }
+public static void Settings() {
+	String line = "Nothing To Change When its perfect.";
+	SlowText(line);
+	System.out.println("\nReturn Home (Any number except 0)");
+	int input = sc.nextInt();
+	if (input < 0 || input > 0){
+		Home();
+	}
+}
+
+public static void Game() {
+		String line = "Nothing here yet.";
+		SlowText(line);
+		System.out.println("\nReturn Home (Any number except 0)");
+		int input = sc.nextInt();
+	}
 }
